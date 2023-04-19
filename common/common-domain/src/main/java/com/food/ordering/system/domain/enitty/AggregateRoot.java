83e3e7a373a8eaba4332b0101a6184f355c048b9
vukien98 <kienkenken3@gmail.com>
2023-04-19 16:51:53 +0700

    push code

---
+++ common/common-domain/src/main/java/com/food/ordering/system/domain/enitty/AggregateRoot.java
@@ -0,0 +1,4 @@
+package com.food.ordering.system.domain.enitty;
+
+public abstract class AggregateRoot<ID> extends BaseEntity<ID> {
+}
---
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/Product.java
@@ -0,0 +1,26 @@
+package com.food.ordering.system.order.service.domain.entity;
+
+import com.food.ordering.system.domain.enitty.BaseEntity;
+import com.food.ordering.system.domain.valueObject.Money;
+import com.food.ordering.system.domain.valueObject.ProductId;
+
+import java.util.UUID;
+
+public class Product extends BaseEntity<ProductId> {
+    private String name;
+    private Money price;
+
+    public Product(ProductId productId, String name, Money price) {
+        super.setId(productId);
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Money getPrice() {
+        return price;
+    }
+}
