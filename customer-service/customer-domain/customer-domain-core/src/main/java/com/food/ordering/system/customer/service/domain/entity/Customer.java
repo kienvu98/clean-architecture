a803d6a37b743805f4f19b4abe1eaea6b6c16007
vukien98 <kienkenken3@gmail.com>
2023-08-11 16:43:32 +0700

    implement customer service

---
+++ customer-service/customer-domain/customer-domain-core/src/main/java/com/food/ordering/system/customer/service/domain/entity/Customer.java
@@ -0,0 +1,29 @@
+package com.food.ordering.system.customer.service.domain.entity;
+
+import com.food.ordering.system.domain.enitty.AggregateRoot;
+import com.food.ordering.system.domain.valueObject.CustomerId;
+
+public class Customer extends AggregateRoot<CustomerId> {
+    private final String userName;
+    private final String firstName;
+    private final String lastName;
+
+    public Customer(CustomerId customerId, String userName, String firstName, String lastName) {
+        super.setId(customerId);
+        this.userName = userName;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}
